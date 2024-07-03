548fdeae61ad1580ee9394d4d86bde1f2a19a21b
Chetan Dhowlaghar <chetan.getsmail@gmail.com>
2024-07-03 17:48:35 -0400

    fetching url from global file

--- Flikr/Main Page/HomeViewModel.swift
+++ Flikr/Main Page/HomeViewModel.swift
@@ -9,7 +9,6 @@ import Foundation
 import Combine
 
 class HomeViewModel: ObservableObject{
-    
     var cancellebles = Set<AnyCancellable>()
     var textCancellebles = Set<AnyCancellable>()
     var apiService: APIServiceProtocol
@@ -26,28 +25,23 @@ class HomeViewModel: ObservableObject{
         $searchText
             .debounce(for: 0.5, scheduler: DispatchQueue.main)
             .sink { text in
-                if !text.isEmpty{
+                if !text.isEmpty {
                     self.callAPI(text: self.searchText)
                 }
             }.store(in: &textCancellebles)
     }
     
     func callAPI(text: String) {
-        let baseURL = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1&tags="
-        let url = baseURL + text
+        let url = Constants.API.baseURL + text
         guard let URL = URL(string: url) else {return}
         fetch(from: URL)
     }
     
-    
-    
     func fetch(from url: URL) {
         cancellebles.removeAll()
         isLoading = true
-        
         let session = URLSession.shared
         let task = session.dataTask(with: url) { data, response, error in
-        
             if let error = error {
                 print("Error: \(error)")
                 return
@@ -56,7 +50,6 @@ class HomeViewModel: ObservableObject{
                 print("No data received")
                 return
             }
-            
             do {
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
