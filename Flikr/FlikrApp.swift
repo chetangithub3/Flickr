e39d41fa2a6b2c6a43c3a44871eb02d2a17eb09b
Chetan Dhowlaghar <chetan.getsmail@gmail.com>
2024-07-03 16:33:05 -0400

    Initial Commit

---
+++ Flikr/FlikrApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlikrApp.swift
+//  Flikr
+//
+//  Created by Chetan Dhowlaghar on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct FlikrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
